e2f95f4010590da325ec4b290de39f7567a7494f
PrantikGhosh <prantikghosh20@gmail.com>
2023-09-09 15:58:26 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e9dd17462amsh0e6ad21ec7e52c2p123c59jsn17db1a9f009f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "601b855d5a1956aef6b68068998e44bc";
